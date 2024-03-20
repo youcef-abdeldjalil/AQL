a21db2e8999d0ab1a0bf397b0d1eb2dee2723976
youcef-abdeldjalil <djalilou.mosta@hotmail.com>
2024-03-20 10:13:28 +0100

    calculatrice test

---
+++ src/main/java/org/example/Calculatrice.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Calculatrice {
+    public int additionner(int a, int b) {
+        result = a + b ;
+        return result;
+    }
+    private int result ;
+
+
+    public String getState() {
+        return "state";
+    }
+}
